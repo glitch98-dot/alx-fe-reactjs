a62cfbad8c519c3905e521fbadf388158199e45d
moses kavoo <moseskavoo98@gmail.com>
2025-08-24 11:20:29 +0300

    Update app files

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,20 +1,26 @@
+// src/App.jsx
 import React, { useState } from "react";
 import PostsComponent from "./components/PostsComponent";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   const [showPosts, setShowPosts] = useState(true);
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>React Query Demo</h1>
-      <button
-        onClick={() => setShowPosts(!showPosts)}
-        style={{ marginBottom: "20px", padding: "6px 12px" }}
-      >
-        {showPosts ? "Hide Posts" : "Show Posts"}
-      </button>
-      {showPosts && <PostsComponent />}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div style={{ padding: "20px" }}>
+        <h1>React Query Demo</h1>
+        <button
+          onClick={() => setShowPosts(!showPosts)}
+          style={{ marginBottom: "20px", padding: "6px 12px" }}
+        >
+          {showPosts ? "Hide Posts" : "Show Posts"}
+        </button>
+        {showPosts && <PostsComponent />}
+      </div>
+    </QueryClientProvider>
   );
 }
 
